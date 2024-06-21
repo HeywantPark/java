98525ca81070dde8a0586b84c94a6583f9e497f6
Park Hyewon <db1648@naver.com>
2024-06-21 17:39:55 +0900

    240621 다향성 기초 예제 문제 풀이

---
+++ study/src/afternoon/poly1/Animalmain.java
@@ -0,0 +1,16 @@
+package afternoon.poly1;
+
+public class Animalmain {
+    public static void main(String[] args) {
+        Donkey donkey = new Donkey();
+        Dog dog = new Dog();
+        Chicken chicken = new Chicken();
+        Cat cat = new Cat();
+
+        donkey.sound();
+        dog.sound();
+        chicken.sound();
+        cat.sound();
+
+    }
+}
