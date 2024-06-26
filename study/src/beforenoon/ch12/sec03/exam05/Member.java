ebd7f7ad419f4ebe6128337cd68587522e3cb009
Park Hyewon <db1648@naver.com>
2024-06-26 12:17:11 +0900

    240626 lombok 라이브러리 설치 및 사용

---
+++ study/src/beforenoon/ch12/sec03/exam05/Member.java
@@ -0,0 +1,17 @@
+package beforenoon.ch12.sec03.exam05;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Member {
+    private String id;
+    private  String name;
+    private  int age;
+}
