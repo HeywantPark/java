90bbb13389079f2add5d16efb21bc6d9e8c6adb6
Park Hyewon <db1648@naver.com>
2024-07-09 15:58:26 +0900

    240709 collection ArrayList , LinkedList 기본

---
+++ study/src/afternoon/collection/list/ArrayExample.java
@@ -0,0 +1,19 @@
+package afternoon.collection.list;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        int[] temp = new int[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+        temp[arr.length] = 6;
+        arr = temp;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
